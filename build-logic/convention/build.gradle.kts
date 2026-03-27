c69abbf38313c5bc1eba240dcb2bda95cd0dbe80
Jared McFarland <jared@mixpanel.com>
2026-03-27 12:38:32 -0700

    Publish real sources jar instead of empty placeholder
    Include actual Kotlin source files in the sources jar so consumers
    can audit the library code via Maven Central. Javadoc jar remains
    empty until Dokka is added for KDoc generation.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     `kotlin-dsl`
 }
 
+dependencies {
+    compileOnly("com.android.tools.build:gradle:8.2.0")
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
